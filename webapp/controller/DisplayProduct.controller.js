32e36038b5258629af81c0433755b8f5b1ef9cfb
Karthick Manna <karthick.manna9999@gmail.com>
2026-06-28 05:00:30 +0000

    New functionality on the Inputs & Simple Form - Class: 28th June, 2026

--- webapp/controller/DisplayProduct.controller.js
+++ webapp/controller/DisplayProduct.controller.js
@@ -1,7 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox",
 ],
-function (Controller) {
+function (Controller, MessageToast,MessageBox) {
     "use strict";
 
     return Controller.extend("salesorderapp.ui.controller.DisplayProduct", {
@@ -17,6 +19,38 @@ function (Controller) {
             var oArgs = oEvent.getParameter("arguments");
             var sProductId = oArgs.ProductId;
             // Do something with the product ID
+        },
+
+        onUpdateProduct: function () {
+            var oSelectedProductModel = this.getOwnerComponent().getModel("SelectedProductModel");
+            var oSelectedProductData = oSelectedProductModel.getData();
+            if(oSelectedProductData.Product_Text==""){
+                MessageBox.warning("Product Name cannot be empty.");
+                MessageToast.show("Product Name cannot be empty.");
+                return;
+            }
+        },
+        onMaximumCapacityChange: function (oEvent) {
+                var oInput = oEvent.getSource();
+                var sValue = oInput.getValue();
+                var iMaxLength = 7; // Set the maximum length to 7 characters
+                if (sValue.length > iMaxLength) {
+                    MessageBox.warning("Maximum Capacity cannot exceed " + iMaxLength + " characters.");
+                    oInput.setValue(sValue.substring(0, iMaxLength)); // Trim the value to the maximum length
+                }
+        },
+
+        onMaximumCapacityLiveChange: function (oEvent) {
+            var oInput = oEvent.getSource();
+            var sValue = oInput.getValue();
+            var iMaxLength = 7; // Set the maximum length to 7 characters
+            if (sValue.length > iMaxLength) {
+                MessageBox.warning("Maximum Capacity cannot exceed " + iMaxLength + " characters.");
+                oInput.setValue(sValue.substring(0, iMaxLength)); // Trim the value to the maximum length
+            }
+            else{
+                MessageToast.show("Maximum Capacity is within the limit of " + iMaxLength + " characters.");
+            }
         }
 
     });
